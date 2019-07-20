88ca91cfc8f18a7b7b9f2399b9377a8a771ee6a2
LILY KAN (ITA-ISDC-ISD-OOCLL/ZHA) <KANLI2@OOCL.COM>
2019-07-20 20:59:14 +0800

    complete vue template

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
+
     <p>{{count}}</p>
     <button @click="add">点我+</button>
     <button @click="sub">点我-</button>
+    <aaa></aaa>
   </div>
 </template>
 
 <script>
+
+import aaa from './components/MutiCount'
 export default {
   name: 'app',
   data:function(){
@@ -15,13 +19,16 @@ export default {
      }
   },
   methods:{
-  add:function(){
+    add:function(){
     this.count++;
-  },
-  sub:function(){
+    },
+    sub:function(){
     this.count--;
+    }
+  },
+  components:{
+    aaa
   }
-}
 };
 </script>
 
